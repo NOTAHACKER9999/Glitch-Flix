a779ff3e5e814f8d9d066ab5a35fe85fccba5cc8
NOTAHACKER9999 <th15isar0bothi@gmail.com>
2025-11-09 15:07:50 -0600

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "glitchflix-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/Icon.png"
+];
+
+// Install service worker and cache assets
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate service worker
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch assets from cache first
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
